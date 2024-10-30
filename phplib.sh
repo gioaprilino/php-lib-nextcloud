7bc470e982de736526c346383a44c88f0ac053d7
Gio <86983840+gioaprilino@users.noreply.github.com>
2024-10-30 23:20:20 +0700

    Add files via upload

---
+++ phplib.sh
@@ -0,0 +1,1 @@
+dnf install php-cli php-fpm php-curl php-mysqlnd php-gd php-opcache php-zip php-intl php-common php-bcmath php-imap php-imagick php-xmlrpc php-json php-readline php-memcached php-redis php-mbstring php-apcu php-xml php-dom php-redis php-memcached php-memcache
